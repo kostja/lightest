1b9230e1cc9c3576539777f30ba0a315870343a4
Konstantin Osipov <kostja@scylladb.com>
2020-05-16 00:11:36 +0300

    pay: begin adding error handling

--- pay.go
+++ pay.go
@@ -9,11 +9,15 @@ import (
 	"math/rand"
 	"runtime"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
-func recover1() error {
-	return nil
+type PayStats struct {
+	errors          uint64
+	no_such_account uint64
+	retries         uint64
+	recoveries      uint64
 }
 
 type Transfer struct {
@@ -34,37 +38,79 @@ func (t *Transfer) New(session *gocql.Session) {
 	t.update_balance = session.Query(UPDATE_BALANCE)
 }
 
-func (t *Transfer) Make(client_id gocql.UUID, src_bic string, src_ban string,
-	dst_bic string, dst_ban string, amount inf.Dec) error {
+func (t *Transfer) Make(
+	client_id gocql.UUID,
+	src_bic string, src_ban string,
+	dst_bic string, dst_ban string,
+	amount inf.Dec, stats *PayStats) error {
+
+	var applied bool
+	var err error
+	row := map[string]interface{}{}
 
 	// Register a new transfer
-	transfer_id := gocql.TimeUUID().String()
+	transfer_id := gocql.TimeUUID()
 	t.insert.Bind(transfer_id, src_bic, src_ban, dst_bic, dst_ban, amount)
-	if err := t.insert.Exec(); err != nil {
+	if applied, err = t.insert.MapScanCAS(row); err != nil {
 		return merry.Wrap(err)
+	}
+	if applied != true {
+		// Should never happen, transfer id is globally unique
+		return merry.New(fmt.Sprintf("Failed to create a transfer %v: a duplicate transfer exists",
+			transfer_id))
 	}
 	// Change transfer state
 	t.update.Bind(client_id, "pending", transfer_id, nil)
-	if err := t.update.Exec(); err != nil {
+	if applied, err = t.update.MapScanCAS(row); err != nil {
 		return merry.Wrap(err)
+	}
+	if applied != true {
+		// Should never happen, noone can pick up our transfer but us
+		return merry.New(fmt.Sprintf("Failed to update transfer %v",
+			transfer_id))
 	}
 	// Lock source account
 	t.check_src_account.Bind(transfer_id, src_bic, src_ban, amount)
-	if err := t.check_src_account.Exec(); err != nil {
+	if applied, err = t.check_src_account.MapScanCAS(row); err != nil {
 		return merry.Wrap(err)
+	}
+	if applied != true {
+		// pending_transfer != null
+		//	 check pending transfer state:
+		//     if the client is there, sleep & restart
+		//     else complete  the existing transfer
+		// insufficient balance
 	}
 	// Lock destination account
 	t.check_dst_account.Bind(transfer_id, dst_bic, dst_ban)
-	if err := t.check_dst_account.Exec(); err != nil {
+	if applied, err = t.check_dst_account.MapScanCAS(row); err != nil {
 		return merry.Wrap(err)
 	}
+	if applied != true {
+		// check pending transfer state:
+		//   if the client is there, sleep & restart
+		//   else complete  the existing transfer
+	}
+	return t.CompleteLockedTransfer(client_id, transfer_id, src_bic, src_ban,
+		dst_bic, dst_ban, amount)
+}
+
+func (t *Transfer) CompleteLockedTransfer(
+	client_id gocql.UUID, transfer_id gocql.UUID,
+	src_bic string, src_ban string,
+	dst_bic string, dst_ban string,
+	amount inf.Dec) error {
+
+	// From now on we can ignore 'applied' - the record may
+	// not be applied only if someone completed our transfer or
+	// 300 seconds have elapsed.
 	// Move transfer to 'in progress', to not attempt to transfer the money
 	// twice
 	t.update.Bind(client_id, "in progress", transfer_id, client_id)
 	if err := t.update.Exec(); err != nil {
 		return merry.Wrap(err)
 	}
-	//
+
 	new_src_balance := amount
 	t.update_balance.Bind(new_src_balance, src_bic, src_ban, transfer_id)
 	if err := t.update_balance.Exec(); err != nil {
@@ -75,7 +121,6 @@ func (t *Transfer) Make(client_id gocql.UUID, src_bic string, src_ban string,
 	if err := t.update_balance.Exec(); err != nil {
 		return merry.Wrap(err)
 	}
-
 	// Move transfer to "complete"
 	t.delete_.Bind(transfer_id, client_id, "in progress")
 	if err := t.delete_.Exec(); err != nil {
@@ -91,7 +136,8 @@ func pay(cmd *cobra.Command, n int) error {
 	if workers > n {
 		workers = n
 	}
-	fmt.Printf("Making %d transfers using %d workers on %d cores \n", n, workers, cores)
+	fmt.Printf("Making %d transfers using %d workers on %d cores \n",
+		n, workers, cores)
 
 	cluster := gocql.NewCluster("localhost")
 	cluster.Authenticator = gocql.PasswordAuthenticator{
@@ -107,6 +153,7 @@ func pay(cmd *cobra.Command, n int) error {
 	if err != nil {
 		return merry.Wrap(err)
 	}
+	var stats PayStats
 
 	worker := func(client_id gocql.UUID, wg *sync.WaitGroup) {
 
@@ -125,9 +172,10 @@ func pay(cmd *cobra.Command, n int) error {
 			dst_bic := gocql.TimeUUID().String()
 			dst_ban := gocql.TimeUUID().String()
 
-			err := transfer.Make(client_id, src_bic, src_ban, dst_bic, dst_ban, *amount)
+			err := transfer.Make(client_id, src_bic, src_ban, dst_bic, dst_ban, *amount, &stats)
 			if err != nil {
 				llog.Printf("%+v", err)
+				atomic.AddUint64(&stats.errors, 1)
 				return
 			}
 		}
@@ -142,7 +190,11 @@ func pay(cmd *cobra.Command, n int) error {
 
 	wg.Wait()
 
-	llog.Printf("Done")
+	fmt.Printf("Errors: %v, Retries: %v, Recoveries: %v, Not found: %v\n",
+		stats.errors,
+		stats.retries,
+		stats.recoveries,
+		stats.no_such_account)
 
 	return nil
 }
